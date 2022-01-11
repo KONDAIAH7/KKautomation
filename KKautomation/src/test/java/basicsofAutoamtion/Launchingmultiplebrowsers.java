027f7b44c2616d4815cc2bf527ffcbe5342da176
kkondaiah1995 <kkondaiah1995@gmail.com>
2022-01-11 06:30:58 +0530

    first comit

---
+++ KKautomation/src/test/java/basicsofAutoamtion/Launchingmultiplebrowsers.java
@@ -0,0 +1,21 @@
+package basicsofAutoamtion;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Launchingmultiplebrowsers {
+
+	public static void main(String[] args) 
+	{
+		WebDriverManager.chromedriver().setup();
+		//Particular version
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+
+	}
+
+}
+ 
